38a7cc9b8db9377b13f04478fecada8a05899a76
Ordenko Vladimir <you@example.com>
2019-12-12 12:07:15 +0300

    Updates

---
+++ precache-manifest.ac579383fc6960fbcef47e6f52136c1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc14365bc60fa2c784560017275a87d4",
+    "url": "/AppQuiz/index.html"
+  },
+  {
+    "revision": "db1446f65ee73ff7f5c5",
+    "url": "/AppQuiz/static/css/main.73c6ce1a.chunk.css"
+  },
+  {
+    "revision": "3e420c562b69a8155fe7",
+    "url": "/AppQuiz/static/js/2.dd5650fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AppQuiz/static/js/2.dd5650fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db1446f65ee73ff7f5c5",
+    "url": "/AppQuiz/static/js/main.21ba8d3c.chunk.js"
+  },
+  {
+    "revision": "94e49b0582acb20f3507",
+    "url": "/AppQuiz/static/js/runtime-main.c5edd90a.js"
+  }
+]);
